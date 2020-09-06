11ada7b412d630dd60e0a3090a4ed3dee8a16a72
”taro <groveleffect@gmail.com>
2020-09-07 08:23:10 +0900

    連絡先一覧の追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="home">
-    <Adresses/>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Adresses from '@/components/Adresses.vue'
 
 export default {
   name: 'Home',
-  components: {
-    Adresses
-  }
+
 }
 </script>
