a1f689f9c6ca0a168360cfc0a8cb7a76d10c473d
”taro <groveleffect@gmail.com>
2020-09-11 12:32:32 +0900

    Googleログイン機能の追加

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@
 </template>
 
 <script>
+import firebase from 'firebase'
 import { mapActions } from 'vuex'
 import SideNav from './components/SideNav'
 
@@ -31,12 +32,19 @@ export default {
   components: {
     SideNav
   },
+  created () {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setLoginUser(user)
+      }
+    })
+  },
 
   data: () => ({
     //
   }),
   methods: {
-    ...mapActions(['toggleSideMenu'])
+    ...mapActions(['toggleSideMenu', 'setLoginUser'])
   }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    login_user: null,
     drawer: false,
     addresses: []
   },
   mutations: {
+    setLoginUser (state, user) {
+      state.login_user = user
+    },
     toggleSideMenu (state) {
       state.drawer = !state.drawer
     },
@@ -17,6 +22,13 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    setLoginUser ( { commit }, user ) {
+      commit('setLoginUser', user)
+    },
+    login () {
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(google_auth_provider)
+    } ,
     toggleSideMenu ({ commit }){
       commit('toggleSideMenu')
     },
